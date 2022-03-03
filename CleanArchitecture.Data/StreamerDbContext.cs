e3b4c870803fab722f87609fa04e812a393a992a
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-03 00:01:57 -0600

    feat: cadena de conexion

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer("Server=localhost;Database=Streamer;User Id=sa;Password=secret123;");
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
