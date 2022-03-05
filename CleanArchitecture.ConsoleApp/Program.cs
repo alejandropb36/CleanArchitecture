218ee2fae10479dd3d06aaeb1871ee353f4a8cc9
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-05 17:21:51 -0600

    feat: consultas condiciones de EF

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -1,42 +1,69 @@
 ﻿using CleanArchitecture.Data;
 using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
 
 StreamerDbContext streamerDbContext = new();
 
-Streamer streamer = new()
+await QueryFilter();
+
+Console.WriteLine("Presiona cualquier tecla para termina");
+Console.ReadKey();
+
+async Task QueryFilter()
 {
-    Nombre = "Amazon Prime",
-    Url = "https://www.amazonprime.com"
-};
+    Console.WriteLine($"Ingresa una compañia de streaming: ");
+    var streamingNombre = Console.ReadLine();
 
-streamerDbContext!.Streamers!.Add(streamer);
-
-await streamerDbContext.SaveChangesAsync();
-
-var movies = new List<Video>()
-{
-    new Video()
+    //var streamers = await streamerDbContext.Streamers.Where(x => x.Nombre == streamingNombre).ToListAsync();
+    var streamers = await streamerDbContext!.Streamers!.Where(x => x.Nombre!.Equals(streamingNombre)).ToListAsync();
+    foreach (var streamer in streamers)
     {
-        Nombre = "Mad Max",
-        StreamerId = streamer.Id
-    },
-    new Video()
-    {
-        Nombre = "Batman",
-        StreamerId = streamer.Id
-    },
-    new Video()
-    {
-        Nombre = "Animales Fantasticos",
-        StreamerId = streamer.Id
-    },
-    new Video()
-    {
-        Nombre = "Forest Gump",
-        StreamerId = streamer.Id
+        Console.WriteLine($"{streamer.Id} - {streamer.Nombre}");
     }
-};
+    
+    
+    //var streamerPartialResults = await streamerDbContext!.Streamers!.Where(x => x.Nombre!.Contains(streamingNombre!)).ToListAsync();
+    var streamerPartialResults = await streamerDbContext!.Streamers!.Where(x => EF.Functions.Like(x.Nombre, $"%{streamingNombre}%")).ToListAsync();
+    foreach (var streamer in streamerPartialResults)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Nombre}");
+    }
+}
 
-await streamerDbContext.AddRangeAsync(movies);
+//Streamer streamer = new()
+//{
+//    Nombre = "Amazon Prime",
+//    Url = "https://www.amazonprime.com"
+//};
 
-await streamerDbContext.SaveChangesAsync();
+//streamerDbContext!.Streamers!.Add(streamer);
+
+//await streamerDbContext.SaveChangesAsync();
+
+//var movies = new List<Video>()
+//{
+//    new Video()
+//    {
+//        Nombre = "Mad Max",
+//        StreamerId = streamer.Id
+//    },
+//    new Video()
+//    {
+//        Nombre = "Batman",
+//        StreamerId = streamer.Id
+//    },
+//    new Video()
+//    {
+//        Nombre = "Animales Fantasticos",
+//        StreamerId = streamer.Id
+//    },
+//    new Video()
+//    {
+//        Nombre = "Forest Gump",
+//        StreamerId = streamer.Id
+//    }
+//};
+
+//await streamerDbContext.AddRangeAsync(movies);
+
+//await streamerDbContext.SaveChangesAsync();
