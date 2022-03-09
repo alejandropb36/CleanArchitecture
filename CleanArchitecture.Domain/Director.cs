6b6cfaa93149d8b4eacb7071278290c130a35812
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-08 20:27:29 -0600

    feat: migracion con nuevas relaciones

--- CleanArchitecture.Domain/Director.cs
+++ CleanArchitecture.Domain/Director.cs
@@ -4,7 +4,9 @@ namespace CleanArchitecture.Domain
 {
     public class Director : BaseDomainModel
     {
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public int VideoId { get; set; }
+        public virtual Video? Video { get; set; }
     }
 }
