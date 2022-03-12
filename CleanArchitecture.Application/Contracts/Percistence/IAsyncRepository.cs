ee4312fa8a978099a8706515f0e28a10b5930d43
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-11 20:51:42 -0600

    feat: expressions vs func

---
+++ CleanArchitecture.Application/Contracts/Percistence/IAsyncRepository.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Domain.Common;
+using System.Linq.Expressions;
+
+namespace CleanArchitecture.Application.Contracts.Percistence
+{
+    public interface IAsyncRepository<T> where T : BaseDomainModel
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+
+    }
+}
