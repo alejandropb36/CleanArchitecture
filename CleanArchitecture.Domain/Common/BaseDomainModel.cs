a4955677b752c12d36a0cf5eae0949ce303e2aa6
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-11 19:40:04 -0600

    feat: modelos con domain drive design

--- CleanArchitecture.Domain/Common/BaseDomainModel.cs
+++ CleanArchitecture.Domain/Common/BaseDomainModel.cs
@@ -4,7 +4,7 @@ namespace CleanArchitecture.Domain.Common
     public abstract class BaseDomainModel
     {
         public int Id { get; set; }
-        public DateTime? DateCreated { get; set; }
+        public DateTime? CreatedDate { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime? LasteModifiedDate { get; set; }
         public string? LastModifiedBy { get; set; }
