263b630d42915372df4bbebdb874ff1dd682bc5f
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-14 20:11:24 -0600

    feat: modelos de capa application

---
+++ CleanArchitecture.Application/Contracts/Percistence/IVideoRepository.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Domain;
+
+namespace CleanArchitecture.Application.Contracts.Percistence
+{
+    public interface IVideoRepository: IAsyncRepository<Video>
+    {
+        Task<IReadOnlyCollection<Video>> GetVideoByName(string nombreVideo);
+    }
+}
