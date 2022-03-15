ed4d6a7f3b43bf63a7b851701bbedc5210d0dac2
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-14 23:03:54 -0600

    feat: implementacion de CQRS

--- CleanArchitecture.Application/Contracts/Percistence/IVideoRepository.cs
+++ CleanArchitecture.Application/Contracts/Percistence/IVideoRepository.cs
@@ -4,6 +4,7 @@ namespace CleanArchitecture.Application.Contracts.Percistence
 {
     public interface IVideoRepository: IAsyncRepository<Video>
     {
-        Task<IReadOnlyCollection<Video>> GetVideoByName(string nombreVideo);
+        Task<Video> GetVideoByName(string nombreVideo);
+        Task<IEnumerable<Video>> GetVideoByUserName(string username);
     }
 }
---
+++ CleanArchitecture.Application/Features/Videos/Queries/GetVideosList/VideoVm.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchitecture.Application.Features.Videos.Queries.GetVideosList
+{
+    public class VideoVm
+    {
+        public string? Nombre { get; set; }
+        public int StreamerId { get; set; }
+    }
+}
