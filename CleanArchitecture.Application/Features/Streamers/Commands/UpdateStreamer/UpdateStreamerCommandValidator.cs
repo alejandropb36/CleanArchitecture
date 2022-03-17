94f6657c433d673bc54c9e22827c133fa84297f9
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-16 21:46:26 -0600

    feat: update streamer command

---
+++ CleanArchitecture.Application/Features/Streamers/Commands/UpdateStreamer/UpdateStreamerCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands.UpdateStreamer
+{
+    public class UpdateStreamerCommandValidator : AbstractValidator<UpdateStreamerCommand>
+    {
+        public UpdateStreamerCommandValidator()
+        {
+            RuleFor(e => e.Nombre)
+                .NotNull().WithMessage("{Nombre} no permite nulos");
+            RuleFor(e => e.Url)
+                .NotNull().WithMessage("{Url} no permite nulos");
+        }
+    }
+}
