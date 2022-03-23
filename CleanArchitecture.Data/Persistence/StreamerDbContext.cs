6b4c1af047372ad0729b951a3fff5f5949ee7697
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-22 20:44:12 -0600

    feat: implementando capa infraestructura

---
+++ CleanArchitecture.Data/Persistence/StreamerDbContext.cs
@@ -0,0 +1,68 @@
+﻿using CleanArchitecture.Domain;
+using CleanArchitecture.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Infraestructure.Persistence
+{
+    public class StreamerDbContext : DbContext
+    {
+        public StreamerDbContext(DbContextOptions<StreamerDbContext> options) : base(options)
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "system";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LasteModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "system";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseSqlServer("Server=localhost;Database=Streamer;User Id=sa;Password=secret123;")
+        //            .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+        //            .EnableSensitiveDataLogging();
+        //    }
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>()
+                .HasMany(s => s.Videos)
+                .WithOne(v => v.Streamer)
+                .HasForeignKey(v => v.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Video>()
+                .HasMany(v => v.Actores)
+                .WithMany(a => a.Videos)
+                .UsingEntity<VideoActor>(
+                    va => va.HasKey(e => new { e.VideoId, e.ActorId })
+                );
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+        public DbSet<Actor>? Actores { get; set; }
+        public DbSet<Director>? Directores { get; set; }
+
+    }
+}
---
+++ CleanArchitecture.Data/Persistence/StreamerDbContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Infraestructure.Persistence
+{
+    public class StreamerDbContextSeed
+    {
+        public static async Task SeedAsync(StreamerDbContext context, ILogger<StreamerDbContextSeed> logger)
+        {
+            if (!context.Streamers!.Any())
+            {
+                var streamers = GetPreconfiguredStreamers();
+                context.Streamers!.AddRange(streamers);
+                await context.SaveChangesAsync();
+                logger.LogInformation("Estamos agregando nuevos records al db {context}", typeof(StreamerDbContext).Name);
+            }
+        }
+
+        private static IEnumerable<Streamer> GetPreconfiguredStreamers()
+        {
+            return new List<Streamer>
+            {
+                new Streamer { CreatedBy = "Alejandro Ponce", Nombre = "Netflix", Url = "http://www.netflix.com" },
+                new Streamer { CreatedBy = "Alejandro Ponce", Nombre = "Amazon Prime", Url = "http://www.amazon-prime.com" },
+                new Streamer { CreatedBy = "Alejandro Ponce", Nombre = "HBO", Url = "http://www.hbo.com" },
+                new Streamer { CreatedBy = "Alejandro Ponce", Nombre = "Disney +", Url = "http://www.disneyplus.com" },
+                new Streamer { CreatedBy = "Alejandro Ponce", Nombre = "Star +", Url = "http://www.starplus.com" },
+                new Streamer { CreatedBy = "Alejandro Ponce", Nombre = "Paramount +", Url = "http://www.paramountplus.com" },
+            };
+        }
+    }
+}
