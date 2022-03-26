886903ebdb757b6a3dc21634753db26f634b6919
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-26 12:30:20 -0600

    feat: implementando meotodos de repositorios especificos

---
+++ CleanArchitecture.Data/Repositories/VideoRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Contracts.Percistence;
+using CleanArchitecture.Domain;
+using CleanArchitecture.Infraestructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infraestructure.Repositories
+{
+    public class VideoRepository : RepositoryBase<Video>, IVideoRepository
+    {
+        public VideoRepository(StreamerDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Video> GetVideoByName(string nombreVideo)
+        {
+            return await _context.Videos!.Where(v => v.Nombre == nombreVideo).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Video>> GetVideoByUserName(string username)
+        {
+            return await _context.Videos!.Where(v => v.CreatedBy == username).ToListAsync();
+        }
+    }
+}
