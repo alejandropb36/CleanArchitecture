4a9e0133ed1eb8919c6d3cd80bbf8c6e5b7522c0
Alejandro Ponce <alexponce36@hotmail.com>
2022-03-30 20:42:39 -0600

    feat: modelos y interfaces de identity

---
+++ CleanArchitecture.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchitecture.Application.Models.Identity;
+
+namespace CleanArchitecture.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(AuthRequest authRequest);
+        Task<RegistrationResponse> Register(RegistrationRequest registrationRequest);
+    }
+}
