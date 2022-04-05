350aaf16c9082cce9a6093a8336ac498055c9b4e
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-04 23:59:38 -0500

    feat: middleware en asp

--- CleanArchitecture.API/Program.cs
+++ CleanArchitecture.API/Program.cs
@@ -1,3 +1,4 @@
+using CleanArchitecture.API.Middlewares;
 using CleanArchitecture.Application;
 using CleanArchitecture.Identity;
 using CleanArchitecture.Infraestructure;
@@ -31,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseAuthentication();
 app.UseAuthorization();
