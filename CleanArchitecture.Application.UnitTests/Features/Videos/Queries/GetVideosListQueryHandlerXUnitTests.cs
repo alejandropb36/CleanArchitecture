1822c952fab0dbf7081197b8d6a04c68106380cb
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-08 16:07:44 -0500

    feat: unit test con ef in memory

--- CleanArchitecture.Application.UnitTests/Features/Videos/Queries/GetVideosListQueryHandlerXUnitTests.cs
+++ CleanArchitecture.Application.UnitTests/Features/Videos/Queries/GetVideosListQueryHandlerXUnitTests.cs
@@ -1,6 +1,37 @@
-﻿namespace CleanArchitecture.Application.UnitTests.Features.Videos.Queries
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Percistence;
+using CleanArchitecture.Application.Features.Videos.Queries.GetVideosList;
+using CleanArchitecture.Application.Mappings;
+using CleanArchitecture.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace CleanArchitecture.Application.UnitTests.Features.Videos.Queries
 {
     public class GetVideosListQueryHandlerXUnitTests
     {
+        private readonly IMapper _mapper;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        public GetVideosListQueryHandlerXUnitTests()
+        {
+            _unitOfWork = MockUnitOfWork.GetUnitOfWork();
+            var mapConfig = new MapperConfiguration(conf => conf.AddProfile<MappingProfile>());
+            _mapper = mapConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetVideoListTest()
+        {
+            var handler = new GetVideosListQueryHandler(_unitOfWork.Object, _mapper);
+            var username = "alejandro";
+            var request = new GetVideosListQuery(username);
+
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            result.ShouldBeOfType<List<VideoVm>>();
+            Assert.True(result.Count > 0);
+        }
     }
 }
--- CleanArchitecture.Application.UnitTests/Mocks/MockVideoRepository.cs
+++ CleanArchitecture.Application.UnitTests/Mocks/MockVideoRepository.cs
@@ -1,19 +1,34 @@
 ﻿using AutoFixture;
-using CleanArchitecture.Application.Contracts.Percistence;
 using CleanArchitecture.Domain;
+using CleanArchitecture.Infraestructure.Persistence;
+using CleanArchitecture.Infraestructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace CleanArchitecture.Application.UnitTests.Mocks
 {
     public static class MockVideoRepository
     {
-        public static Mock<IVideoRepository> GetVideoRepository()
+        public static Mock<VideoRepository> GetVideoRepository()
         {
             var fixture = new Fixture();
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
             var videos = fixture.CreateMany<Video>().ToList();
+            videos.Add(
+                fixture.Build<Video>()
+                .With(tr => tr.CreatedBy, "alejandro")
+                .Create()
+            );
 
-            var videoMockRepository = new Mock<IVideoRepository>();
-            videoMockRepository.Setup(r => r.GetAllAsync()).ReturnsAsync(videos);
+            var options = new DbContextOptionsBuilder<StreamerDbContext>()
+                .UseInMemoryDatabase(databaseName: $"StreamerDbContext-{Guid.NewGuid()}")
+                .Options;
+
+            var streamerDbContextFake = new StreamerDbContext(options);
+            streamerDbContextFake.Videos!.AddRange(videos);
+            streamerDbContextFake.SaveChanges();
+
+            var videoMockRepository = new Mock<VideoRepository>(streamerDbContextFake);
             return videoMockRepository;
         }
     }
