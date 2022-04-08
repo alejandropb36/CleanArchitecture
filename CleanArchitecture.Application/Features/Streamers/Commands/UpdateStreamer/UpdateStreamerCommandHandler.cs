191abcdb30d34fc06f4ceb5ab24c2e6ab90ffe7b
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-07 23:17:13 -0500

    feat: unit of work en los features

--- CleanArchitecture.Application/Features/Streamers/Commands/UpdateStreamer/UpdateStreamerCommandHandler.cs
+++ CleanArchitecture.Application/Features/Streamers/Commands/UpdateStreamer/UpdateStreamerCommandHandler.cs
@@ -9,20 +9,21 @@ namespace CleanArchitecture.Application.Features.Streamers.Commands.UpdateStream
 {
     public class UpdateStreamerCommandHandler : IRequestHandler<UpdateStreamerCommand>
     {
-        private readonly IStreamerRepository _streamerRepository;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly ILogger<UpdateStreamerCommandHandler> _logger;
 
-        public UpdateStreamerCommandHandler(IStreamerRepository streamerRepository, IMapper mapper, ILogger<UpdateStreamerCommandHandler> logger)
+        public UpdateStreamerCommandHandler(IUnitOfWork unitOfWork, IMapper mapper, ILogger<UpdateStreamerCommandHandler> logger)
         {
-            _streamerRepository = streamerRepository;
+            _unitOfWork = unitOfWork;
             _mapper = mapper;
             _logger = logger;
         }
 
         public async Task<Unit> Handle(UpdateStreamerCommand request, CancellationToken cancellationToken)
         {
-            var streamer = await _streamerRepository.GetByIdAsync(request.Id);
+            var streamer = await _unitOfWork.StreamerRepository.GetByIdAsync(request.Id);
+
             if (streamer == null)
             {
                 _logger.LogError($"No se encontro el streamer id {request.Id}");
@@ -31,7 +32,13 @@ namespace CleanArchitecture.Application.Features.Streamers.Commands.UpdateStream
 
             _mapper.Map(request, streamer, typeof(UpdateStreamerCommand), typeof(Streamer));
 
-            await _streamerRepository.UpdateAsync(streamer);
+            _unitOfWork.StreamerRepository.UpdateEntity(streamer);
+            var result = await _unitOfWork.Complete();
+
+            if (result <= 0)
+            {
+                throw new Exception($"El record streamer no se pudo actualizar"); 
+            }
 
             _logger.LogInformation($"La operacion fue exitosa actualizando el streamer {request.Id}");
 
--- CleanArchitecture.Application/Features/Videos/Queries/GetVideosList/GetVideosListQueryHandler.cs
+++ CleanArchitecture.Application/Features/Videos/Queries/GetVideosList/GetVideosListQueryHandler.cs
@@ -6,18 +6,18 @@ namespace CleanArchitecture.Application.Features.Videos.Queries.GetVideosList
 {
     public class GetVideosListQueryHandler : IRequestHandler<GetVideosListQuery, List<VideoVm>>
     {
-        private readonly IVideoRepository _videoRepository;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
-        public GetVideosListQueryHandler(IVideoRepository videoRepository, IMapper mapper)
+        public GetVideosListQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
         {
-            _videoRepository = videoRepository;
+            _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
 
         public async Task<List<VideoVm>> Handle(GetVideosListQuery request, CancellationToken cancellationToken)
         {
-            var videoList = await _videoRepository.GetVideoByUserName(request.Username);
+            var videoList = await _unitOfWork.VideoRepository.GetVideoByUserName(request.Username);
             
 
             return _mapper.Map<List<VideoVm>>(videoList);
