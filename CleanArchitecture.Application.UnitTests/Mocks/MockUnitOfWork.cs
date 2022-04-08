9d58f938324d85ba9649ae6b8d824b4603ddb881
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-07 23:45:42 -0500

    feat: creando mocks de unit of work

--- CleanArchitecture.Application.UnitTests/Mocks/MockUnitOfWork.cs
+++ CleanArchitecture.Application.UnitTests/Mocks/MockUnitOfWork.cs
@@ -1,6 +1,17 @@
-﻿namespace CleanArchitecture.Application.UnitTests.Mocks
+﻿using CleanArchitecture.Application.Contracts.Percistence;
+using Moq;
+
+namespace CleanArchitecture.Application.UnitTests.Mocks
 {
-    public class MockUnitOfWork
+    public static class MockUnitOfWork
     {
+        public static Mock<IUnitOfWork> GetUnitOfWork()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var mockVideoRepository = MockVideoRepository.GetVideoRepository();
+
+            mockUnitOfWork.Setup(r => r.VideoRepository).Returns(mockVideoRepository.Object);
+            return mockUnitOfWork;
+        }
     }
 }
--- CleanArchitecture.Application.UnitTests/Mocks/MockVideoRepository.cs
+++ CleanArchitecture.Application.UnitTests/Mocks/MockVideoRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace CleanArchitecture.Application.UnitTests.Mocks
+﻿using AutoFixture;
+using CleanArchitecture.Application.Contracts.Percistence;
+using CleanArchitecture.Domain;
+using Moq;
+
+namespace CleanArchitecture.Application.UnitTests.Mocks
 {
-    public class MockVideoRepository
+    public static class MockVideoRepository
     {
+        public static Mock<IVideoRepository> GetVideoRepository()
+        {
+            var fixture = new Fixture();
+            var videos = fixture.CreateMany<Video>().ToList();
+
+            var videoMockRepository = new Mock<IVideoRepository>();
+            videoMockRepository.Setup(r => r.GetAllAsync()).ReturnsAsync(videos);
+            return videoMockRepository;
+        }
     }
 }
